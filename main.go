6ee06128ae699e2328d4c33350ad66e7e584afb5
Filippo Mameli <mameli93@gmail.com>
2023-09-17 14:34:09 +0200

    rm main code

--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-// import "go_advent/calories"
-// import "go_advent/rps"
-// import "go_advent/rucksack"
-
 func main() {
-	// calories.Count_cal()
-	// rps.Get_final_score()
-	// rucksack.Get_priority_sum()
-
+	println("Hello world!")
 }
